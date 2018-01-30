41151464604e2ccc5e629a990f6130245c6b7cab
Gen <gki.penguin@gmail.com>
2018-01-30 20:39:41 +0900

    refactor: Add files to support command line call

---
+++ pretty-git-merges/command_line.py
@@ -0,0 +1,10 @@
+"""Command line entry point."""
+import pretty_git_merges
+
+
+def main():
+    """Call pretty_git_merges.main."""
+    pretty_git_merges.main()
+
+if __name__ == '__main__':
+    main()
