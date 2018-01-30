88ec6a889efd94702ef1dd0dda369f53ccd536f0
Gen <gki.penguin@gmail.com>
2018-01-30 18:32:51 +0900

    build: Add setup.py for pypi.

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""Setup.py for pip distribution."""
+
+from setuptools import setup
+
+setup(
+    name='pretty-git-merges',
+    version='0.1.0',
+    description='Command line tool to obtain formatted git merge log.',
+    url='https://github.com/gki/pretty-git-merges',
+    author='Gen Takeda',
+    author_email='gki.penguin@gmail.com',
+    license='MIT',
+    keywords='git merges log formatted pretty release note',
+    packages=[
+        "pretty-git-merges",
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
